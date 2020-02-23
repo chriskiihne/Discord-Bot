67bb17bd70492cad3117ae8cf2b53f899d6f09bf
chriskiihne <46977523+chriskiihne@users.noreply.github.com>
2020-02-22 20:45:39 -0600

    adding the actual files to repo

---
+++ discord bot/index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.member.roles.find(r => r.name === 'BOT')) {
+		message.channel.send('Shut the fuck up bot ' + message.member.user.tag);
+	}
+});
+
+client.login('NjgwODY3NTAzNzQ3ODkxMjE1.XlGJSg.Fex3X38zDPBl6iCV6vxoikDyy14');
